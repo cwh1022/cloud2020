3005cac318610d4c173d72992fc52fe980572c14
connor.chen <cwh1022@qq.com>
2020-03-20 11:12:21 +0800

    Eureka

--- cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
+++ cloud-eureka-server7002/src/main/java/com/atguigu/springcloud/EurekaMain7002.java
@@ -2,17 +2,17 @@ package com.atguigu.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- * Created on 2020/3/19
+ * Created on 2020/3/20
  *
  * @author connor.chen
  */
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain8001 {
+@EnableEurekaServer
+public class EurekaMain7002 {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain8001.class, args);
+        SpringApplication.run(EurekaMain7002.class, args);
     }
 }
